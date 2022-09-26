8d0b19d3bb0a08988a0d3c717e009cd368b4f6dc
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2022-09-26 13:18:29 +0200

    added primaryColor change to example

--- example/src/App.js
+++ example/src/App.js
@@ -1,3 +1,10 @@
+import {Button} from 'semantic-ui-react'
 export function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Button primary>This button is red because the primaryColor was changed in site.variables</Button>
+    </div>
+
+  )
 }
